3743457fef62eb6ccebf796a1a0a2a8d765d4f76
Wimsb <radmirradmir324@gmail.com>
2025-10-22 15:57:03 +0500

    8.1 ended

--- MurzayanovAutoservice/SignUpPage.xaml.cs
+++ MurzayanovAutoservice/SignUpPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,6 +23,8 @@ namespace MurzayanovAutoservice
     {
         private Service _currentService = new Service();
 
+        private ClientService _currentClientService = new ClientService();
+
         public SignUpPage(Service SelectedService)
         {
             InitializeComponent();
@@ -32,9 +35,57 @@ namespace MurzayanovAutoservice
             }
 
             DataContext = _currentService;
+
+            var _currentClient = MurzayanovEntities.GetContext().Client.ToList();
+
+            ComboClient.ItemsSource = _currentClient;
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (ComboClient.SelectedItem == null)
+            {
+                errors.AppendLine("Укажите ФИО клиента.");
+            }
+            if (StartDate.Text == "")
+            {
+                errors.AppendLine("Укажите дату услуги.");
+            }
+            if (TBStart.Text == "")
+            {
+                errors.AppendLine("Укажите время начала услуги.");
+            }
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            _currentClientService.ClientID = ComboClient.SelectedIndex+1;
+            _currentClientService.ServiceID = _currentService.ID;
+            _currentClientService.StartTime = Convert.ToDateTime(StartDate.Text + " " + TBStart.Text);
+
+            if (_currentClientService.ID == 0)
+            {
+                MurzayanovEntities.GetContext().ClientService.Add(_currentClientService);
+            }
+
+            try
+            {
+                MurzayanovEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена.");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void TBStart_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
